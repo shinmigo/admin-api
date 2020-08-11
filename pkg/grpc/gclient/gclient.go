3b5e0f0bb75c4871b21e269f201830df047e0041
will <815115659@qq.com>
2020-08-11 17:44:58 +0800

    连接grpc服务优化

--- pkg/grpc/gclient/gclient.go
+++ pkg/grpc/gclient/gclient.go
@@ -2,11 +2,12 @@ package gclient
 
 import (
 	"fmt"
-	"github.com/shinmigo/pb/memberpb"
 	"goshop/api/pkg/grpc/etcd3"
 	"goshop/api/pkg/utils"
 	"log"
 	"strings"
+
+	"github.com/shinmigo/pb/memberpb"
 
 	"github.com/shinmigo/pb/productpb"
 
@@ -19,6 +20,22 @@ var ProductParam productpb.ParamServiceClient
 var Member memberpb.MemberServiceClient
 
 func DialGrpcService() {
+	pms()
+	crm()
+}
+
+func crm() {
+	r := etcd3.NewResolver(utils.C.Etcd.Host)
+	resolver.Register(r)
+	conn, err := grpc.Dial(r.Scheme()+"://author/"+utils.C.Grpc.Name["crm"], grpc.WithBalancerName("round_robin"), grpc.WithInsecure())
+	if err != nil {
+		log.Panicf("grpc没有连接上%s, err: %v \n", utils.C.Grpc.Name["crm"], err)
+	}
+	fmt.Printf("连接成功：%s, host分别为: %s \n", utils.C.Grpc.Name["crm"], strings.Join(utils.C.Etcd.Host, ","))
+	Member = memberpb.NewMemberServiceClient(conn)
+}
+
+func pms() {
 	r := etcd3.NewResolver(utils.C.Etcd.Host)
 	resolver.Register(r)
 
@@ -30,24 +47,4 @@ func DialGrpcService() {
 	fmt.Printf("连接成功：%s, host分别为: %s \n", utils.C.Grpc.Name["pms"], strings.Join(utils.C.Etcd.Host, ","))
 	ProductTag = productpb.NewTagServiceClient(conn)
 	ProductParam = productpb.NewParamServiceClient(conn)
-
-	connCrm, err := grpc.Dial(r.Scheme()+"://author/"+utils.C.Grpc.Name["crm"], grpc.WithBalancerName("round_robin"), grpc.WithInsecure())
-	if err != nil {
-		log.Panicf("grpc没有连接上%s, err: %v \n", utils.C.Grpc.Name["crm"], err)
-	}
-	fmt.Printf("连接成功：%s, host分别为: %s \n", utils.C.Grpc.Name["crm"], strings.Join(utils.C.Etcd.Host, ","))
-	Member = memberpb.NewMemberServiceClient(connCrm)
-
-	//for {
-	//	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	//	resp, err := Hello.Echo(ctx, &productpb.Payload{Data: "hello"}, grpc.FailFast(true))
-	//	cancel()
-	//	if err != nil {
-	//		fmt.Println(err, "err---")
-	//	} else {
-	//		fmt.Println(resp)
-	//	}
-	//
-	//	<-time.After(time.Second)
-	//}
 }
