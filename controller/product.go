e28605486412fcfead1a8aa8e66dc864e2186053
Sam <19040137@shdm.com>
2020-08-24 14:13:18 +0800

    商品服务添加

--- controller/product.go
+++ controller/product.go
@@ -12,6 +12,16 @@ func (m *Product) Initialise() {
 	ProductFilter = filter.NewProduct(m.Context)
 }
 
+func (m *Product) Index() {
+	list, err := ProductFilter.Index()
+	if err != nil {
+		m.SetResponse(nil, err)
+		return
+	}
+
+	m.SetResponse(list)
+}
+
 func (m *Product) Add() {
 	err := ProductFilter.Add()
 	if err != nil {
@@ -24,6 +34,16 @@ func (m *Product) Add() {
 
 func (m *Product) Edit() {
 	err := ProductFilter.Edit()
+	if err != nil {
+		m.SetResponse(nil, err)
+		return
+	}
+
+	m.SetResponse()
+}
+
+func (m *Product) Delete() {
+	err := ProductFilter.Delete()
 	if err != nil {
 		m.SetResponse(nil, err)
 		return
