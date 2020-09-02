97a4c64dcc4a758d659fcdc46b92464139efcfa3
shrimp <1732341@qq.com>
2020-09-02 18:02:44 +0800

    member 添加认证

--- router/member.go
+++ router/member.go
@@ -4,11 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 	"goshop/api/controller"
 	"goshop/api/pkg/core/routerhelper"
+	"goshop/api/pkg/middleware"
 )
 
 func init() {
 	routerhelper.Use(func(r *gin.Engine) {
-		g := routerhelper.NewGroupRouter("member", new(controller.Member), r)
+		g := routerhelper.NewGroupRouter("member", new(controller.Member), r, middleware.VerifyToken())
 		g.Get("/index")
 		g.Post("/add")
 		g.Post("/edit")
